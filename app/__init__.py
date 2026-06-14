b32bbb57949c08602077198407ab7a50fc1ce96e
jgarciaakiyama <jgarciaakiyama@ecom.com.ar>
2026-06-14 11:16:37 -0300

    Agrego Bootstrap y layout principal

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,14 @@
 from flask import Flask
 
 from app.routes.conductor import conductor_bp
+from app.routes.home import home_bp
 
 
 def create_app():
 
     app = Flask(__name__)
 
-    app.register_blueprint(
-        conductor_bp
-    )
+    app.register_blueprint(conductor_bp)
+    app.register_blueprint(home_bp)
 
     return app
