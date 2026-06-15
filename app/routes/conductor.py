444b052407435a5221c5e050c26ad6c6652d12a3
jgarciaakiyama <jgarciaakiyama@ecom.com.ar>
2026-06-15 00:21:36 -0300

    Primer formulario para la carga de distancia y corriente

--- app/routes/conductor.py
+++ app/routes/conductor.py
@@ -1,13 +1,22 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+
 
 conductor_bp = Blueprint(
     "conductor",
     __name__
 )
 
-@conductor_bp.route("/conductor")
+@conductor_bp.route("/conductor",methods=["GET", "POST"])
 def conductor():
+    corriente = None 
+    distancia = None
+    
+    if request.method == "POST": 
+        corriente = request.form.get( "corriente" ) 
+        distancia = request.form.get( "distancia" )
 
     return render_template(
-        "conductor.html"
+        "conductor.html",
+        corriente=corriente,
+        distancia=distancia
     )
