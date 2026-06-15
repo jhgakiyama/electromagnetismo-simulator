ae08c78bbf06616c0aeeb101023245177a10b199
jgarciaakiyama <jgarciaakiyama@ecom.com.ar>
2026-06-15 01:22:43 -0300

    Se agrega la resolucion paso a paso

--- app/routes/conductor.py
+++ app/routes/conductor.py
@@ -13,19 +13,25 @@ def conductor():
     corriente = None 
     distancia = None
     resultado = None
+    pasos = []
 
     if request.method == "POST": 
         corriente = float(request.form.get("corriente"))
         distancia = float(request.form.get("distancia"))
-        resultado = calcular_campo_magnetico(corriente,distancia)
-        resultado_formateado = (formatear_cientifico(resultado))
+        datos_calculo = calcular_campo_magnetico(corriente,distancia)
+
+        resultado = datos_calculo["resultado"]
+        pasos = datos_calculo["pasos"]
+
+        resultado_formateado = formatear_cientifico(resultado)
 
     return render_template(
         "conductor.html",
         corriente=corriente,
         distancia=distancia,
         resultado=resultado,
-        resultado_formateado=resultado_formateado
+        resultado_formateado=resultado_formateado,
+        pasos=pasos
     )
 
 
--- app/services/conductor_service.py
+++ app/services/conductor_service.py
@@ -4,5 +4,14 @@ MU_0 = 4 * math.pi * 1e-7
 
 def calcular_campo_magnetico(corriente,distancia):
     campo = (MU_0 * corriente) / (2 * math.pi * distancia)
-    return campo
-
+    
+    # Muestro los pasos para la proxima seccion
+    pasos = [
+        "B = μ₀ · I / (2π · r)",
+        f"B = ({MU_0:.6e} × {corriente}) / (2π × {distancia})", # puede ser mejor a la vista >> 4π × 10⁻⁷
+        f"B = {campo:.6e} T"
+    ]
+    
+    return { 
+        "resultado": campo,"pasos": pasos
+    }
