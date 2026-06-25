d20adb6938b4dbc6f6a6ebd10afb6c447ae9013b
jgarciaakiyama <jgarciaakiyama@ecom.com.ar>
2026-06-25 07:28:06 -0300

    cambio el grafico conceptual 2 para ver en el template demo

--- app/routes/tema2.py
+++ app/routes/tema2.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from flask import Blueprint
-from app.plots.tema2_plot import visualizacion_conceptual_2a
+from app.plots.tema2_plot import visualizacion_conceptual_2a, visualizacion_conceptual_2b
 
 bp = Blueprint(
     "tema2",
@@ -10,7 +10,8 @@ bp = Blueprint(
 @bp.route("/tema2/demo")
 def tema2_demo():
 
-    fig = visualizacion_conceptual_2a()
+    # fig = visualizacion_conceptual_2a()
+    fig = visualizacion_conceptual_2b()
 
     return render_template(
         "tema2_demo.html",
