9b135466159d30db07ee3d13c7597bbe438feb2e
jgarciaakiyama <jgarciaakiyama@ecom.com.ar>
2026-06-28 12:40:10 -0300

    Nuevo template y ruta para el ejercicio 2 parte A

--- app/routes/tema2.py
+++ app/routes/tema2.py
@@ -2,7 +2,10 @@ from flask import render_template
 from flask import Blueprint
 from app.plots.tema2_plot_vista_concep1 import visualizacion_conceptual_2a
 from app.plots.tema2_plot_vista_concep2 import visualizacion_conceptual_2b
-from app.plots.tema2_plot import visualizacion_ejercicio_2
+from app.plots.tema2_plot import (
+    visualizacion_ejercicio_2, 
+    visualizacion_resolucion_p1
+    )
 
 
 bp = Blueprint(
@@ -29,6 +32,19 @@ def ejercicio():
 
     return render_template(
         "tema2_ejercicio.html",
+        grafico=fig.to_html(
+            full_html=False,
+            include_plotlyjs="cdn"
+        )
+    )
+
+@bp.route("/tema2/resolucion/p1")
+def resolucion_p1():
+
+    fig = visualizacion_resolucion_p1()
+
+    return render_template(
+        "tema2_resolucion_p1.html",
         grafico=fig.to_html(
             full_html=False,
             include_plotlyjs="cdn"
