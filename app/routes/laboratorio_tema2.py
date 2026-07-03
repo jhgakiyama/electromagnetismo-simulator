b3011340ed9ba170ff23436a7e3b857fe62b1f88
jgarciaakiyama <jgarciaakiyama@ecom.com.ar>
2026-07-03 00:06:18 -0300

    Se genera grafico en la ruta. Se corrige error que generaba valores como tuplas

--- app/routes/laboratorio_tema2.py
+++ app/routes/laboratorio_tema2.py
@@ -1,9 +1,8 @@
 from flask import Blueprint
 from flask import render_template
 from flask import request
-
-
 from app.physics.laboratorio import calcular_simulacion
+from app.plots.laboratorio_plot import visualizacion_resultado_laboratorio
 
 
 bp = Blueprint(
@@ -23,27 +22,29 @@ def tema2_laboratorio():
         px = float(request.form["px"])
         py = float(request.form["py"])
     else:
-        corriente1=1.0,
-        sentido1="entrante",
-        corriente2=1.0,
-        sentido2="entrante",
-        px=1.0,
+        corriente1=1.0
+        sentido1="entrante"
+        corriente2=1.0
+        sentido2="entrante"
+        px=1.0
         py=1.0
     
     resultado = calcular_simulacion(
-        corriente1=1,
-        sentido1="entrante",
-        corriente2=1,
-        sentido2="entrante",
-        px=1,
-        py=1
+        corriente1=corriente1,
+        sentido1=sentido1,
+        corriente2=corriente2,
+        sentido2=sentido2,
+        px=px,
+        py=py
     )
+    print(resultado["parametros"])
     
-    
+    grafico = visualizacion_resultado_laboratorio(resultado)
+
     return render_template(
         template,
         resultado=resultado,
-        grafico=resultado["grafico"].to_html(
+        grafico=grafico.to_html(
             full_html=False,
             include_plotlyjs="cdn"
         )
