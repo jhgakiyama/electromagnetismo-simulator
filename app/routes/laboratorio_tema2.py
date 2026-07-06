241d3bd6c40cc4c7694f629f212209b637f64d76
jgarciaakiyama <jgarciaakiyama@ecom.com.ar>
2026-07-06 10:30:50 -0300

    Mejoras en la ruta para incluir 3 estados posibles. Primera vez , simulación es correcta o el experimento es inválido

--- app/routes/laboratorio_tema2.py
+++ app/routes/laboratorio_tema2.py
@@ -1,6 +1,4 @@
-from flask import Blueprint
-from flask import render_template
-from flask import request
+from flask import Blueprint,render_template, request
 from app.physics.laboratorio import calcular_simulacion
 from app.plots.laboratorio_plot import visualizacion_resultado_laboratorio
 
@@ -12,9 +10,25 @@ bp = Blueprint(
 
 @bp.route("/tema2/laboratorio", methods=["GET", "POST"])
 def tema2_laboratorio():
+       
+    # ==========================================================
+    # Estado inicial
+    # ==========================================================
     template = "tema2_laboratorio.html"
-    mensaje_error = None
+    resultado = None
     grafico = None
+    grafico_html = None
+    mensaje_error = None
+
+    # ==========================================================
+    # Valores por defecto del formulario
+    # ==========================================================
+    corriente1=1.0
+    corriente2=1.0
+    sentido1="entrante"
+    sentido2="entrante"
+    px=1.0
+    py=1.0
 
     if request.method == "POST":
         corriente1 = float(request.form["corriente1"])
@@ -23,36 +37,36 @@ def tema2_laboratorio():
         sentido2 = request.form["sentido2"]
         px = float(request.form["px"])
         py = float(request.form["py"])
-    else:
-        corriente1=1.0
-        sentido1="entrante"
-        corriente2=1.0
-        sentido2="entrante"
-        px=1.0
-        py=1.0
-    try:
-        resultado = calcular_simulacion(
-            corriente1=corriente1,
-            sentido1=sentido1,
-            corriente2=corriente2,
-            sentido2=sentido2,
-            px=px,
-            py=py
-        )
-    except ValueError as e:
-        resultado = None
-        mensaje_error = str(e) 
-    # print(resultado["parametros"])
-    
-    if resultado is not None:
-        grafico = visualizacion_resultado_laboratorio(resultado)
+        
+        try:
+            resultado = calcular_simulacion(
+                corriente1=corriente1,
+                sentido1=sentido1,
+                corriente2=corriente2,
+                sentido2=sentido2,
+                px=px,
+                py=py
+            )
+
+            grafico = visualizacion_resultado_laboratorio(resultado)
+            grafico_html = grafico.to_html(
+                full_html=False,
+                include_plotlyjs="cdn"
+            )
+
+        except ValueError as e:
+
+            mensaje_error = str(e) 
 
     return render_template(
         template,
         resultado=resultado,
-        grafico=grafico.to_html(
-            full_html=False,
-            include_plotlyjs="cdn"
-        ),
-        mensaje_error=mensaje_error
+        grafico=grafico_html,
+        mensaje_error=mensaje_error,
+        corriente1=corriente1,
+        corriente2=corriente2,
+        sentido1=sentido1,
+        sentido2=sentido2,
+        px=px,
+        py=py
     )
