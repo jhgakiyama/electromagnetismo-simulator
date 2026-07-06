9295acb5efe06526ebc80218a79184ca4f47ad04
jgarciaakiyama <jgarciaakiyama@ecom.com.ar>
2026-07-06 11:43:01 -0300

    Se mejora el mensaje de la excepcion

--- app/physics/validaciones.py
+++ app/physics/validaciones.py
@@ -16,9 +16,12 @@ def validar_geometria_experimento(r1, r2):
     if r1 < EPSILON :
         raise ValueError(
             "El punto de observación coincide con el conductor I1."
+            "En esa posición el modelo del conductor rectilíneo infinito presenta una singularidad (r = 0), por lo que no es posible calcular el campo magnético."
+            
         )
 
     if r2 < EPSILON :
         raise ValueError(
             "El punto de observación coincide con el conductor I2."
+            "En esa posición el modelo del conductor rectilíneo infinito presenta una singularidad (r = 0), por lo que no es posible calcular el campo magnético."
         )
