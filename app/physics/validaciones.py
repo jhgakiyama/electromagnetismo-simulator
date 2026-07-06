b2b545a983beda5ff0a22356366630a8f55ec985
jgarciaakiyama <jgarciaakiyama@ecom.com.ar>
2026-07-06 09:26:31 -0300

    Agrego una contantes para evitar errores de precision numerica con valores muy pequeños

--- app/physics/validaciones.py
+++ app/physics/validaciones.py
@@ -1,3 +1,6 @@
+EPSILON = 1e-9
+
+
 def validar_geometria_experimento(r1, r2):
     """
     Valida la geometría del experimento antes
@@ -10,12 +13,12 @@ def validar_geometria_experimento(r1, r2):
         con alguno de los conductores.
     """
     
-    if r1 == 0:
+    if r1 < EPSILON :
         raise ValueError(
             "El punto de observación coincide con el conductor I1."
         )
 
-    if r2 == 0:
+    if r2 < EPSILON :
         raise ValueError(
             "El punto de observación coincide con el conductor I2."
         )
